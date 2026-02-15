fa650c73a8e1f915194d830e29d9d0a27fbab7ae
kirill-antonovich <kirill.a.shchipanov@gmail.com>
2026-02-15 23:49:04 +0100

    Updates

--- frontend/src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://well-analytics.onrender.com",
-});
-
-export default api;
--- frontend/src/components/InfoBox.jsx
+++
@@ -1,7 +0,0 @@
-import { COLORS } from "../constants/colors"
-
-export default function InfoBox({ text }) {
-	return (
-		<p className="m-3 pr-1 pl-1 border-2 rounded-full text-xs" style={{ borderColor: COLORS.SECONDARY, color: COLORS.SECONDARY }} title={text}><i><b>i</b></i></p>
-	)
-}
